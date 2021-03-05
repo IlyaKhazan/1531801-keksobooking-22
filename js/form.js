5ea14cbf752115d8f0c7e8fd66439b21af577562
IlyaKhazan <p9067700518@gmail.com>
2021-03-05 20:06:21 +0300

    ДЗ 6.2

--- js/form.js
+++ js/form.js
@@ -1,15 +1,20 @@
-import { typeMinCosts } from './data.js';
+import { typeMinCosts, roomCapacity } from './data.js';
 
 const form = document.querySelector('.ad-form');
 const filters = document.querySelector('.map__filters');
 const addFormElements = Array.from(form.elements);
 const addFilterElements = Array.from(filters.elements);
 
+const title = form.querySelector('#title');
 const price = form.querySelector('#price');
 const type = form.querySelector('#type');
+
 const checkIn = form.querySelector('#timein');
 const checkOut = form.querySelector('#timeout');
 const checkInOutGroup = form.querySelector('.ad-form__element--time');
+
+const roomNumber = form.querySelector('#room_number');
+const capacity = form.querySelector('#capacity');
 
 const toggleElements = (elements, state) => {
   elements.forEach((item) => item.disabled = state);
@@ -29,9 +34,9 @@ const activatePage = () => {
   toggleElements(addFilterElements, false);
 };
 
-const typePriceFilterChange = (evt) => {
-  price.min = typeMinCosts[evt.target.value];
-  price.placeholder = typeMinCosts[evt.target.value];
+const typePriceFilterChange = () => {
+  price.min = typeMinCosts[type.value];
+  price.placeholder = typeMinCosts[type.value];
 }
 
 const checkInOutChange = (evt) => {
@@ -39,7 +44,23 @@ const checkInOutChange = (evt) => {
   checkOut.value = evt.target.value;
 }
 
+const roomCapacityFilterChange = () => {
+  capacity.innerHTML = roomCapacity[roomNumber.value];
+}
+
 type.addEventListener('change', typePriceFilterChange);
 checkInOutGroup.addEventListener('change', checkInOutChange);
+roomNumber.addEventListener('change', roomCapacityFilterChange);
+
+title.addEventListener('input', () => {
+  title.reportValidity();
+});
+
+price.addEventListener('input', () => {
+  price.reportValidity();
+});
+
+typePriceFilterChange();
+roomCapacityFilterChange();
 
 export { activatePage, deactivatePage }
